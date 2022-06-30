def4529cbbac234a4e44dd176acf88b2e3e8c115
anton litvak <anton.k.litvak@gmail.com>
2022-06-30 18:18:38 +0200

    popup for nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-//import logo from "./logo.svg";
 import "./App.css";
 import MyCalendar from "./Components/Calendar";
 import Event from "./Components/Event";
+import React from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 function App() {
   return (
@@ -10,9 +12,48 @@ function App() {
         <a className="active" href="#home">
           Home
         </a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <Popup
+          trigger={<button className="navBtn">News</button>}
+          position="bottom left"
+        >
+          <div>
+            Cupcake ipsum dolor sit amet cotton candy apple pie caramels muffin.
+            Powder topping bear claw bear claw danish ice cream croissant gummi
+            bears macaroon. Bear claw bear claw topping pastry liquorice jelly.
+            Muffin jelly beans icing cupcake jelly-o. Brownie candy canes sesame
+            snaps macaroon shortbread biscuit dessert caramels. Lollipop sugar
+            plum fruitcake muffin powder sweet roll. Caramels tootsie roll
+            cheesecake wafer tart ice cream apple pie.
+          </div>
+        </Popup>
+        <Popup
+          trigger={<button className="navBtn">Contact</button>}
+          position="bottom center"
+        >
+          <div>
+            Cupcake ipsum dolor sit amet cotton candy apple pie caramels muffin.
+            Powder topping bear claw bear claw danish ice cream croissant gummi
+            bears macaroon. Bear claw bear claw topping pastry liquorice jelly.
+            Muffin jelly beans icing cupcake jelly-o. Brownie candy canes sesame
+            snaps macaroon shortbread biscuit dessert caramels. Lollipop sugar
+            plum fruitcake muffin powder sweet roll. Caramels tootsie roll
+            cheesecake wafer tart ice cream apple pie.
+          </div>
+        </Popup>
+        <Popup
+          trigger={<button className="navBtn">About</button>}
+          position="bottom right"
+        >
+          <div>
+            Cupcake ipsum dolor sit amet cotton candy apple pie caramels muffin.
+            Powder topping bear claw bear claw danish ice cream croissant gummi
+            bears macaroon. Bear claw bear claw topping pastry liquorice jelly.
+            Muffin jelly beans icing cupcake jelly-o. Brownie candy canes sesame
+            snaps macaroon shortbread biscuit dessert caramels. Lollipop sugar
+            plum fruitcake muffin powder sweet roll. Caramels tootsie roll
+            cheesecake wafer tart ice cream apple pie.
+          </div>
+        </Popup>
       </div>
       <h1 id="mainTitle">All Cycling Events - Berlin</h1>
       <div id="calendarContainer">
